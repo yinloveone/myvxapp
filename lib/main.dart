6ead0464bbf8210ea20aeee8eaa408e3d7f0d838
Jack Wang <864655687@qq.com>
2020-05-29 11:12:28 +0800

    first upload

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import './app.dart';
+import './loading.dart';
+
+void main() => runApp(MyApp(
+));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  return MaterialApp(
+  debugShowCheckedModeBanner:false,
+  title: 'wechart',
+  theme: mDefaultTheme,
+  routes: <String,WidgetBuilder>{
+    "/friends":(_) => new WebviewScaffold(
+      url: "https://wexin.qq.com",
+      appBar: AppBar(title:new Text("微信朋友圈")),
+      withZoom: true,
+      withLocalStorage: true,
+    ),
+    //'search:(BuildContext context) => new Search(),
+  },
+
+);
+  }
+}
+final ThemeData mDefaultTheme = new ThemeData(
+  primaryColor:Color(0xff303030),
+  scaffoldBackgroundColor:Color(0xFFebebeb),
+  cardColor:Color(0xff393a3f),
+
+);
+
